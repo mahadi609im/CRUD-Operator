b837a4fc240515f93f15365b11c516bbebc5513d
Mahadi Hasan Milon <maha609im@gmail.com>
2024-09-17 06:35:43 +0600

    Catch input & button
    Catching input field and add button, and When the add button is clicked the input value will be printed

--- crud/js/main.js
+++ crud/js/main.js
@@ -1,1 +1,6 @@
+let inputBox = document.getElementById('inputBox')
 
+document.getElementById('addBtn').addEventListener("click", ()=>{
+   console.log('i am clicked')
+   console.log(inputBox.value)
+})
